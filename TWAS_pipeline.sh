985426efd75e6339473baf7fdc712843d6294f53
Nitchakan Obma <nitchakanobma@gmail.com>
2026-04-26 22:51:23 -0400

    add multi-tissue TWAS pipeline

--- TWAS_pipeline.sh
+++ TWAS_pipeline.sh
@@ -29,7 +29,6 @@ conda activate imlabtools
 # Install additional dependencies not in imlabtools by default
 pip install matplotlib # for Manhattan and QQ plots
 
-
 # Test both tools work
 /opt/anaconda3/envs/imlabtools/bin/python MetaXcan/software/SPrediXcan.py --help
 /opt/anaconda3/envs/imlabtools/bin/python MetaXcan/software/SMulTiXcan.py --help
@@ -53,9 +52,9 @@ ls data/model/eqtl/elastic_net/ | grep -E "Uterus|Ovary|Vagina|Adipose|Whole_Blo
 wget "https://zenodo.org/records/3518299/files/gtex_v8_expression_mashr_snp_smultixcan_covariance.txt.gz" \
   -O data/model/eqtl/mashr/gtex_v8_expression_mashr_snp_smultixcan_covariance.txt.gz
 
-# Get the UCSC genome band coordinate for each gene and chromosome location from Table 1 - for step 7 
+# Get the UCSC genome band coordinate for each gene and chromosome location from Table 1 - for step 7
 wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/cytoBand.txt.gz \
-  -O data/cytoband/cytoBand.txt.gzgunzip cytoBand.txt.gz
+  -O data/cytoband/cytoBand.txt.gz
 
 gzcat data/cytoband/cytoBand.txt.gz | awk '
   ($1=="chr3"  && $4=="q21.3")  ||
@@ -65,7 +64,7 @@ gzcat data/cytoband/cytoBand.txt.gz | awk '
   ($1=="chr17" && $4=="q21.32") ' > data/cytoband/chr_band.txt
 
 # == AIM 1 – GWAS QC ==========
-# Filters: autosomes only, biallelic SNPs, palindromic SNP removal, 
+# Filters: autosomes only, biallelic SNPs, palindromic SNP removal,
 # MHC exclusion (chr6:26-34Mb), deduplication, SE/p sanity checks
 # Constructs varID (chr_pos_OA_EA_b38) and varID_flip columns for mashr
 
@@ -84,40 +83,42 @@ gzcat data/gwas/allEC_aim1.gz | head -6
 # View QC summary
 cat results/aim1/qc_summary.txt
 
-#LDSC SNP heritability (run on BU SCC)
+# LDSC SNP heritability + LD clumping (run on BU SCC)
 # Copy files to SCC
-scp data/gwas/allEC_ldsc.gz nobma@scc1.bu.edu:/projectnb/bs859/students/nobma/final_proj/
-scp run_ldsc.sh nobma@scc1.bu.edu:/projectnb/bs859/students/nobma/final_proj/
+scp data/gwas/allEC_ldsc.gz path/to/file/on/scc/
+scp run_ldsc.sh path/to/file/on/scc/
+scp ld_clumping.sh path/to/file/on/scc/
 
 # On BU SCC run:
+./ld_clumping.sh 2>&1 | tee results/aim1/plink_clump.log
 ./run_ldsc.sh 2>&1 | tee results/aim1/ldsc_run.log
 
 # Copy results back to local
-scp "nobma@scc1.bu.edu:/projectnb/bs859/students/nobma/final_proj/results/aim1/*" results/aim1/
+scp "path/to/file/on/scc/*" results/aim1/
 
 
 # == AIM 2 – S-PrediXcan (elastic net, 6 tissues) ==========
 # Outputs:  results/aim2/spredixcan_allEC_{TISSUE}.csv for each tissue
 
-# make a directory for aim 2's results 
+# make a directory for aim 2's results
 mkdir -p results/aim2
 
 # run S-PrediXcan Elastic Net Model
 ./run_spredixcan_en.sh 2>&1 | tee results/aim2/spredixcan_en_run.log
 
 # view the s-predixcan elastic net results
-python view_spredixcan.py
+python view_spredixcan_en.py
 
 # AIM 2 – S-MultiXcan (joint 6-tissue, elastic net)
 # Output:   results/aim2/smultixcan_allEC.csv, results/aim2/smultixcan_allEC.log
-./run_smultixcan.sh
+./run_smultixcan_en.sh
 
 # check the output files
 head -5 results/aim2/smultixcan_allEC.csv
 wc -l results/aim2/smultixcan_allEC.csv
 
-# view the the s-multixcan results of the elastic net 
-python view_smultixcan.py
+# view the s-multixcan results of the elastic net
+python view_smultixcan_en.py
 
 
 # AIM 3 – MASHR S-PrediXcan (6 tissues)
@@ -131,6 +132,15 @@ mkdir -p results/aim3/mashr
 # run S-PrediXcan Mashr Model
 ./run_spredixcan_mashr.sh 2>&1 | tee results/aim3/mashr/spredixcan_mashr_run.log
 
+# view the s-predixcan mashr results
+python view_spredixcan_mashr.py
+
+# AIM 3 – S-MultiXcan (joint 6-tissue, mashr)
+# Output:   results/aim3/smultixcan_mashr.csv
+./run_smultixcan_mashr.sh
+
+# view the s-multixcan results of the mashr
+python view_smultixcan_mashr.py
 
 # AIM 3 – Compare elastic net vs mashr results
 # Produces 4 comparison tables:
